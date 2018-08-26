179ad822f7cc10499a5e75f3df74322c79b81bae
Paul Leavitt <pleavitt@gmail.com>
2018-08-26 21:51:25 +1000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.scss';
+
+const App = () => (
+  <div>
+    <h3>Demo Webpack 4 + Babel + scss + eslint + react</h3>
+  </div>
+);
+export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
